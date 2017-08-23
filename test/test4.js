19f996ba0599c1a6ccb95d1900c7b5d000a6bbea
Chen <chiyanc@amazon.com>
2017-08-22 17:14:29 -0700

    * Add support for continuation local storage.

---
+++ test/test4.js
@@ -0,0 +1,53 @@
+Error.stackTraceLimit = Infinity;
+
+var sync = require('../lib/main').proc3;
+var co = sync.proc;
+var implicit = sync.implicit;
+var letImplicit = sync.letImplicit;
+var getState = sync.getState;
+var setState = sync.setState;
+var tame = sync.tame;
+
+var mul = function(a, b, cb) {
+    setTimeout(function() {
+        cb(null, a*b);
+    }, 1000);
+};
+
+var square = co(function*(a) {
+    var coeff = yield getState('coeff');
+    console.log(coeff);
+    return coeff * (yield tame(mul)(a, a));
+});
+
+var squareSum = co(function*(a, b) {
+    var a2 = yield square(a);
+    var b2 = yield square(b);
+    return Math.sqrt(a2 + b2);
+});
+
+var main = co(function*(list) {
+    yield setState('coeff', 100);
+
+    var res = 0;
+
+    for (var i = 0; i < list.length; i++) {
+        var tmp = yield square(list[i]);
+        res = res + tmp;
+    }
+
+    return res;
+});
+
+
+var cb = function(s, err, res) {
+    if (err) {
+        console.log('cb ERROR', err.stack);
+        console.log(err.__generatorStack__);
+    } else {
+        console.log('OK', res);
+    }
+};
+
+// squareSum(3,4)({a:2}, {}, cb);
+main([3,4])({a:2}, {}, cb);
