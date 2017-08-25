498817e62e20aca2072e53bb7971bfd6190c2af0
redblaze <redblaze2005@gmail.com>
2017-08-25 03:26:49 -0700

    * Add parallel

---
+++ test/test5.js
@@ -0,0 +1,38 @@
+var sync = require('../lib/main').sync3;
+var co = sync.proc;
+var implicit = sync.implicit;
+var letImplicit = sync.letImplicit;
+var getState = sync.getState;
+var setState = sync.setState;
+var lift = sync.lift;
+var parallel = sync.parallel;
+
+var sleep = co(function*(time) {
+    yield function(e, s, cb) {
+        setTimeout(function() {
+            cb(s);
+        }, time);
+    };
+});
+
+var printSequence = co(function*(s) {
+    for (var i = 0; i < 100; i++) {
+        console.log(s + i);
+        yield sleep(500 * Math.random());
+    }
+});
+
+var main = co(function*() {
+    yield parallel([printSequence('a'), printSequence('b')]);
+});
+
+var cb = function(s, err, res) {
+    if (err) {
+        console.log('cb ERROR', err.stack);
+        console.log(err.__generatorStack__);
+    } else {
+        console.log('OK', res);
+    }
+};
+
+main()({}, {}, cb);
